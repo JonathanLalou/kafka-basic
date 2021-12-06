888f7be95696cbe98fa544c8ff8ebd514a210d32
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2021-12-06 13:35:00 -0500

    init repository

---
+++ src/test/java/com/github/jonathanlalou/kafkabasic/KafkaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jonathanlalou.kafkabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
