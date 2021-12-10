841bae6e2971cbfb93a7fd7f1ce71cdef65ada75
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2021-12-09 21:07:15 -0500

    access local DB without authentication

--- src/main/java/com/github/jonathanlalou/kafkabasic/controller/KafkaBasicController.java
+++ src/main/java/com/github/jonathanlalou/kafkabasic/controller/KafkaBasicController.java
@@ -1,10 +1,13 @@
 package com.github.jonathanlalou.kafkabasic.controller;
 
+import com.github.jonathanlalou.kafkabasic.domain.Letter;
+import com.github.jonathanlalou.kafkabasic.repository.LetterRepository;
 import com.github.jonathanlalou.kafkabasic.service.KafkaBasicEmitter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +22,16 @@ public class KafkaBasicController {
         logger.info("GET /hello");
         return kafkaBasicEmitter.sendMessagesToKafka().get();
     }
+
+    // DELETEME
+    @Autowired
+    private LetterRepository letterRepository;
+    @GetMapping("/letter")
+    @ResponseBody
+    public Letter letter() throws Exception {
+        logger.info("GET /letter");
+        return letterRepository.save(new Letter(999, 'X', 1, 2, 3, 4, 5, 6));
+    }
+
 
 }
