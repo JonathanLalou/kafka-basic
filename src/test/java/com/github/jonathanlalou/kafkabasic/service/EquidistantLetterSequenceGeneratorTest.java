5aa2daa79e60737170e97149e0faf1c4f137c87e
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2021-12-14 15:49:15 -0500

    fixed unit test

--- src/test/java/com/github/jonathanlalou/kafkabasic/service/EquidistantLetterSequenceGeneratorTest.java
+++ src/test/java/com/github/jonathanlalou/kafkabasic/service/EquidistantLetterSequenceGeneratorTest.java
@@ -1,17 +1,18 @@
-package com.github.jonathanlalou.kafkabasic.batch;
+package com.github.jonathanlalou.kafkabasic.service;
 
 
 import com.github.jonathanlalou.kafkabasic.domain.EquidistantLetterSequence;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class EquidistantLetterSequenceGeneratorTaskletTest {
+public class EquidistantLetterSequenceGeneratorTest {
 
     private String allLetters = StringUtils.remove("""
                     AndwhatdoyouthinkofthislatestcomedythecoronationatMilanaskedAnnaPavlovnaandofthecomedyofthepeopleofGenoaandLuccalayingtheirpetitionsbefore
@@ -53,29 +54,19 @@ public class EquidistantLetterSequenceGeneratorTaskletTest {
                     """
             , '\n');
 
-    private EquidistantLetterSequenceGeneratorTasklet equidistantLetterSequenceGeneratorTasklet = new EquidistantLetterSequenceGeneratorTasklet();
+    private EquidistantLetterSequenceGenerator equidistantLetterSequenceGenerator = new EquidistantLetterSequenceGenerator();
     private int minInterval = 1;
     private int maxInterval = 100;
 
-    @BeforeEach
-    void setUp() {
-        equidistantLetterSequenceGeneratorTasklet.setMinInterval(minInterval);
-        equidistantLetterSequenceGeneratorTasklet.setMaxInterval(maxInterval);
-        equidistantLetterSequenceGeneratorTasklet.setAllLetters(allLetters);
-
-    }
-
     @Test
     public void generateEquidistantLetterSequences() {
-        final List<EquidistantLetterSequence> equidistantLetterSequences = equidistantLetterSequenceGeneratorTasklet.generateEquidistantLetterSequences(minInterval, maxInterval, allLetters);
-        for (int i = 0; i < 10; i++) {
-            System.out.println(ToStringBuilder.reflectionToString(equidistantLetterSequences.get(i)));
-        }
-        final EquidistantLetterSequence element0 = equidistantLetterSequences.get(0);
-        final EquidistantLetterSequence element1 = equidistantLetterSequences.get(1);
-        final EquidistantLetterSequence element2 = equidistantLetterSequences.get(2);
-        final EquidistantLetterSequence element3 = equidistantLetterSequences.get(3);
-        final EquidistantLetterSequence elementLast = equidistantLetterSequences.get(equidistantLetterSequences.size()-1);
+        final List<EquidistantLetterSequence> actual = equidistantLetterSequenceGenerator.generateEquidistantLetterSequences(minInterval, maxInterval, allLetters);
+
+        final EquidistantLetterSequence element0 = actual.get(0);
+        final EquidistantLetterSequence element1 = actual.get(1);
+        final EquidistantLetterSequence element2 = actual.get(2);
+        final EquidistantLetterSequence element3 = actual.get(3);
+        final EquidistantLetterSequence elementLast = CollectionUtils.lastElement(actual);
 
         assertAll(
                 () -> assertEquals(allLetters, element0.getContent())
@@ -90,8 +81,10 @@ public class EquidistantLetterSequenceGeneratorTaskletTest {
                 , () -> assertTrue(element3.getContent().startsWith("AwtytnfiasodhoniainkAavvaohodfeoeGonuaygepiobo"))
                 , () -> assertEquals(1, element3.getFirstLetter())
 
-                , () -> assertTrue(elementLast.getContent().startsWith("Annesou"))
+                , () -> assertTrue(elementLast.getContent().startsWith("GnryvafreaemltteauefrMtaPPgineocpttnaldomitwsdr"))
                 , () -> assertEquals(100, elementLast.getFirstLetter())
+
+                , () -> assertEquals((maxInterval * (maxInterval + 1)) / 2, actual.size())
         );
     }
 
