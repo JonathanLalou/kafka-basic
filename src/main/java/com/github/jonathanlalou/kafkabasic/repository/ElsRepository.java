06ea37104bb01e338dfcb5adf5ff9e58a6b48681
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2021-12-15 18:07:02 -0500

    added method

--- src/main/java/com/github/jonathanlalou/kafkabasic/repository/ElsRepository.java
+++ src/main/java/com/github/jonathanlalou/kafkabasic/repository/ElsRepository.java
@@ -5,6 +5,9 @@ import com.github.jonathanlalou.kafkabasic.domain.Letter;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ElsRepository extends MongoRepository<Els, Integer> {
+    List<Els> findByContentContains(String word);
 }
