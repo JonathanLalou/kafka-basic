bdcef196dfcaaa01b867c200f76a013015aa9ba9
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2021-12-15 18:12:55 -0500

    added step

--- src/main/java/com/github/jonathanlalou/kafkabasic/config/BatchConfig.java
+++ src/main/java/com/github/jonathanlalou/kafkabasic/config/BatchConfig.java
@@ -2,7 +2,8 @@ package com.github.jonathanlalou.kafkabasic.config;
 
 import com.github.jonathanlalou.kafkabasic.batch.ElsGeneratorTasklet;
 import com.github.jonathanlalou.kafkabasic.batch.ElsSendToKafkaTasklet;
-import com.github.jonathanlalou.kafkabasic.batch.GrossDataSendToKafkaTasklet;
+import com.github.jonathanlalou.kafkabasic.batch.GhardaiaCleaningTasklet;
+import com.github.jonathanlalou.kafkabasic.batch.LetterSendToKafkaTasklet;
 import com.github.jonathanlalou.kafkabasic.batch.JsonFileLoadTasklet;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -21,9 +22,10 @@ import org.springframework.context.annotation.Profile;
 @Profile("feed")
 public class BatchConfig {
     public static final String JSON_FILE_LOAD_TASKLET_STEP = "jsonFileLoadTaskletStep";
-    public static final String GROSS_DATA_SEND_TO_KAFKA_TASKLET_STEP = "grossDataSendToKafkaTaskletStep";
+    public static final String LETTER_SEND_TO_KAFKA_STEP = "letterSendToKafkaStep";
     public static final String ELS_GENERATOR_STEP = "elsGeneratorStep";
     public static final String ELS_SEND_TO_KAFKA_STEP = "ElsSendToKafkaStep";
+    public static final String GHARDAIA_CLEANING_STEP = "ghardaiaCleaningStep";
     @Autowired
     public JobBuilderFactory jobBuilderFactory;
 
@@ -33,14 +35,16 @@ public class BatchConfig {
     @Bean
     public Job job(
             @Qualifier(JSON_FILE_LOAD_TASKLET_STEP) Step jsonFileLoadTaskletStep
-            , @Qualifier(GROSS_DATA_SEND_TO_KAFKA_TASKLET_STEP) Step grossDataSendToKafkaTaskletStep
+            , @Qualifier(LETTER_SEND_TO_KAFKA_STEP) Step letterSendToKafkaStep
             , @Qualifier(ELS_GENERATOR_STEP) Step elsGeneratorStep
             , @Qualifier(ELS_SEND_TO_KAFKA_STEP) Step elsSendToKafkaStep
+            , @Qualifier(GHARDAIA_CLEANING_STEP) Step ghardaiaCleaningStep
     ) {
         return jobBuilderFactory
                 .get("taskletsJob")
-                .start(jsonFileLoadTaskletStep)
-                .next(grossDataSendToKafkaTaskletStep)
+                .start(ghardaiaCleaningStep)
+                .next(jsonFileLoadTaskletStep)
+                .next(letterSendToKafkaStep)
                 .next(elsGeneratorStep)
                 .next(elsSendToKafkaStep)
                 .build();
@@ -56,13 +60,13 @@ public class BatchConfig {
                 .build();
     }
 
-    @Bean(GROSS_DATA_SEND_TO_KAFKA_TASKLET_STEP)
+    @Bean(LETTER_SEND_TO_KAFKA_STEP)
     protected Step grossDataSendToKafkaTaskletStep(
-            GrossDataSendToKafkaTasklet grossDataSendToKafkaTasklet
+            LetterSendToKafkaTasklet letterSendToKafkaTasklet
     ) {
         return stepBuilderFactory
-                .get(GROSS_DATA_SEND_TO_KAFKA_TASKLET_STEP)
-                .tasklet(grossDataSendToKafkaTasklet)
+                .get(LETTER_SEND_TO_KAFKA_STEP)
+                .tasklet(letterSendToKafkaTasklet)
                 .build();
     }
 
@@ -83,6 +87,16 @@ public class BatchConfig {
         return stepBuilderFactory
                 .get(ELS_SEND_TO_KAFKA_STEP)
                 .tasklet(elsSendToKafkaTasklet)
+                .build();
+    }
+
+    @Bean(GHARDAIA_CLEANING_STEP)
+    protected Step ghardaiaCleaningStep(
+            GhardaiaCleaningTasklet ghardaiaCleaningTasklet
+    ) {
+        return stepBuilderFactory
+                .get(GHARDAIA_CLEANING_STEP)
+                .tasklet(ghardaiaCleaningTasklet)
                 .build();
     }
 }
