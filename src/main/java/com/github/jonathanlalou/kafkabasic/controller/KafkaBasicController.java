b06ef8c4052fb4c1ebcbb12843dab89f5a885ce8
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2021-12-15 18:08:35 -0500

    added REST entrypoint

--- src/main/java/com/github/jonathanlalou/kafkabasic/controller/KafkaBasicController.java
+++ src/main/java/com/github/jonathanlalou/kafkabasic/controller/KafkaBasicController.java
@@ -1,39 +1,51 @@
 package com.github.jonathanlalou.kafkabasic.controller;
 
+import com.github.jonathanlalou.kafkabasic.domain.Els;
 import com.github.jonathanlalou.kafkabasic.domain.Letter;
+import com.github.jonathanlalou.kafkabasic.repository.ElsRepository;
 import com.github.jonathanlalou.kafkabasic.repository.LetterRepository;
-import com.github.jonathanlalou.kafkabasic.service.KafkaBasicEmitter;
+import com.github.jonathanlalou.kafkabasic.service.LetterKafkaProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 public class KafkaBasicController {
-    private static final Logger logger = LoggerFactory.getLogger(KafkaBasicController.class);
+//    @Autowired
+//    private LetterKafkaProducer letterKafkaProducer;
 
-    @Autowired
-    private KafkaBasicEmitter kafkaBasicEmitter;
-
-    @GetMapping("/hello")
-    public String hello() throws Exception {
-        logger.info("GET /hello");
-        return kafkaBasicEmitter.sendMessagesToKafka(List.of()).get();
-    }
-
-    // DELETEME
     @Autowired
     private LetterRepository letterRepository;
+    @Autowired
+    private ElsRepository elsRepository;
 
-    @GetMapping("/letter")
+    @GetMapping("/hello")
     @ResponseBody
-    public Letter letter() throws Exception {
-        logger.info("GET /letter");
-        return letterRepository.save(new Letter(999, 'X', 'X', 1, 2, 3, 4, 5, 6, 7, false));
+    public String hello() throws Exception {
+        log.info("GET /hello");
+        return "hello world!";
+    }
+
+//    @GetMapping("/letter")
+//    @ResponseBody
+//    public Letter letter() throws Exception {
+//        log.info("GET /letter");
+//        return letterRepository.save(new Letter(999, 'X', 'X', 1, 2, 3, 4, 5, 6, 7, false));
+//    }
+
+    @GetMapping("/search/{word}")
+    @ResponseBody
+    public List<Els> letter(@RequestParam String word) throws Exception {
+        log.info("GET /search/{}", word);
+        return elsRepository.findByContentContains(word);
     }
 
 
