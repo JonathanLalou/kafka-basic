f91d6671f8737367cdb257fe2ec1338df2483282
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2021-12-23 15:23:15 -0500

    cleaning

--- src/main/java/com/github/jonathanlalou/kafkabasic/controller/GhardaiaController.java
+++ src/main/java/com/github/jonathanlalou/kafkabasic/controller/GhardaiaController.java
@@ -38,12 +38,6 @@ public class GhardaiaController {
         return ghardaiaService.search(word, max, Integer.parseInt(DEFAULT_RANGE));
     }
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
-
     @GetMapping(value = "/find/{word}")
     public String find(
             @PathVariable("word") String word
