9ff76f1d488413f2ef35c7ccf832938553a57665
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2021-12-23 12:24:23 -0500

    nothing

--- src/main/java/com/github/jonathanlalou/kafkabasic/service/GhardaiaService.java
+++ src/main/java/com/github/jonathanlalou/kafkabasic/service/GhardaiaService.java
@@ -32,7 +32,6 @@ import javax.annotation.PostConstruct;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
